c172b710f35f4590330ee45c38602a51bbedaa01
Guillermo Teran <guijomter@gmail.com>
2025-11-28 19:43:39 -0300

    Compe 03 creación de target y FE

--- src/config.py
+++ src/config.py
@@ -39,7 +39,7 @@ def load_yaml_config(file_path=PATH_CONFIG):
 conf = load_yaml_config(PATH_CONFIG)
 
 ## Disponibilizar variables globales de la competencia01 para acceso directo (no hace falta poner conf.competencia01. delante)
-cfg = conf.competencia02
+cfg = conf.competencia03
 globals().update(cfg.__dict__)
 
 

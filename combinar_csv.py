627d5d6d411f44c7db31cf531470ac47eaa4491c
Guillermo Teran <guijomter@gmail.com>
2025-11-29 00:26:58 -0300

    combinar con polars

--- combinar_csv.py
+++ combinar_csv.py
@@ -1,11 +1,13 @@
 import gzip
 import polars as pl
+import shutil
 
 
 BUCKET= "/home/guijomter/buckets/b1/datasets"
 csv_file_1 = f"{BUCKET}/competencia_02_crudo.csv.gz"
 csv_file_2 = f"{BUCKET}/competencia_03_crudo.csv.gz"
 csv_file_out = f"{BUCKET}/competencia_03_crudo_comp.csv.gz"
+csv_file_out_uncompressed = f"{BUCKET}/competencia_03_crudo_comp.csv"
 
 df1 = pl.read_csv(csv_file_1, infer_schema_length=None , try_parse_dates=False)
 df2 = pl.read_csv(csv_file_2, infer_schema_length=None , try_parse_dates=False)
@@ -14,11 +16,13 @@ df2 = pl.read_csv(csv_file_2, infer_schema_length=None , try_parse_dates=False)
 df = pl.concat([df1, df2], how="vertical")
 
 # Guardar limpio
-#df.write_csv(csv_file_out, compression="gzip")
+df.write_csv(csv_file_out_uncompressed)
 
 
-with gzip.open(csv_file_out, "wt", encoding="utf-8") as f:
-    df.write_csv(f)
+with open(csv_file_out_uncompressed, "rb") as f_in:
+    with gzip.open(csv_file_out, "wb") as f_out:
+        shutil.copyfileobj(f_in, f_out)
+
 
 
 

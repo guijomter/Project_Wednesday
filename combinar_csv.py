50273fd3ec7f06ed69d2e6e2badb06bbbe20c35d
Guillermo Teran <guijomter@gmail.com>
2025-11-29 00:03:54 -0300

    combinar con polars

--- combinar_csv.py
+++ combinar_csv.py
@@ -7,8 +7,8 @@ csv_file_1 = f"{BUCKET}/competencia_02_crudo.csv.gz"
 csv_file_2 = f"{BUCKET}/competencia_03_crudo.csv.gz"
 csv_file_out = f"{BUCKET}/competencia_03_crudo_comp.csv.gz"
 
-df1 = pl.read_csv(csv_file_1, infer_schema_length=10000 , try_parse_dates=False)
-df2 = pl.read_csv(csv_file_2, infer_schema_length=10000 , try_parse_dates=False)
+df1 = pl.read_csv(csv_file_1, infer_schema_length=None , try_parse_dates=False)
+df2 = pl.read_csv(csv_file_2, infer_schema_length=None , try_parse_dates=False)
 
 # Concatenar
 df = pl.concat([df1, df2], how="vertical")

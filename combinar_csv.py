99009a79752b4c8a1618db3833a067a458b20d57
Guillermo Teran <guijomter@gmail.com>
2025-11-28 23:54:45 -0300

    combinar con polars

--- combinar_csv.py
+++ combinar_csv.py
@@ -1,15 +1,30 @@
 import gzip
+import polars as pl
+
 
 BUCKET= "/home/guijomter/buckets/b1/datasets"
 csv_file_1 = f"{BUCKET}/competencia_02_crudo.csv.gz"
 csv_file_2 = f"{BUCKET}/competencia_03_crudo.csv.gz"
 csv_file_out = f"{BUCKET}/competencia_03_crudo_comp.csv.gz"
 
-with gzip.open(csv_file_out, "wt") as fout:
-    with gzip.open(csv_file_1, "rt") as f1:
-        fout.write(f1.read())
+df1 = pl.read_csv(csv_file_1, try_parse_dates=False)
+df2 = pl.read_csv(csv_file_2, try_parse_dates=False)
 
-    with gzip.open(csv_file_2, "rt") as f2:
-        # Saltar el header del segundo archivo
-        next(f2)
-        fout.write(f2.read())
+# Concatenar
+df = pl.concat([df1, df2], how="vertical")
+
+# Guardar limpio
+df.write_csv(csv_file_out, compression="gzip")
+
+
+
+
+
+# with gzip.open(csv_file_out, "wt") as fout:
+#     with gzip.open(csv_file_1, "rt") as f1:
+#         fout.write(f1.read())
+
+#     with gzip.open(csv_file_2, "rt") as f2:
+#         # Saltar el header del segundo archivo
+#         next(f2)
+#         fout.write(f2.read())
